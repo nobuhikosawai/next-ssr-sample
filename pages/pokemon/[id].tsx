c8e4ee2dcef886b66620c5b77216a61b7e7d7e0d
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-03-23 02:33:12 +0900

    detail page

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -4,59 +4,58 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 
-type Pokemon = {
-  name: string;
-};
-
 type Props = {
-  pokemons: Pokemon[];
+  name: string;
+  imageUrl: string;
 };
 
 export default function Home({
-  pokemons,
+  name,
+  imageUrl,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { t } = useTranslation("common");
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">{t("pokemon")}</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="p-4">
-        <ul>
-          {pokemons.map((pokemon) => {
-            return (
-              <li key={pokemon.name} className="underline">
-                <Link href={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <h2 className="text-xl">{t("pokemon")}</h2>
+
+        <div>
+          <p>
+            {t("name")}: {name}
+          </p>
+          <img src={imageUrl} />
+        </div>
       </div>
+
+      <Link href="/">Back to home</Link>
 
       <div className="pt-4 lg:hidden">Your are on mobile</div>
     </div>
   );
 }
 
-type Result = {
-  name: string;
-};
-
 type Response = {
-  next: string;
-  count: number;
-  results: Result[];
+  name: string;
+  sprites: {
+    back_default: string;
+  };
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({
+  query,
   locale,
 }) => {
+  const { id } = query;
   const { data } = await axios.get<Response>(
-    `https://pokeapi.co/api/v2/pokemon`
+    `https://pokeapi.co/api/v2/pokemon/${id}`
   );
 
   // Pass data to the page via props
   const _props: Props = {
-    pokemons: data.results,
+    name: data.name,
+    imageUrl: data.sprites.back_default,
     ...(await serverSideTranslations(locale ?? "ja", ["common"])),
   };
   return { props: _props };
