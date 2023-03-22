374be3cbbd5c5974d79c9a3c0ce173c6b584cc75
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-03-23 02:13:01 +0900

    list pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,58 +2,56 @@ import axios from "axios";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
-import Link from "next/link";
+
+type Pokemon = {
+  name: string;
+};
 
 type Props = {
-  name: string;
-  imageUrl: string;
+  pokemons: Pokemon[];
 };
 
 export default function Home({
-  name,
-  imageUrl,
+  pokemons,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { t } = useTranslation("common");
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold">{t("pokemon")}</h1>
       <div className="p-4">
-        <h2 className="text-xl">{t("pokemon")}</h2>
-
-        <div>
-          <p>
-            {t("name")}: {name}
-          </p>
-          <img src={imageUrl} />
-        </div>
+        <ul>
+          {pokemons.map((pokemon) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>;
+          })}
+        </ul>
       </div>
 
       <div className="pt-4 lg:hidden">Your are on mobile</div>
-
-      <Link href="/another_index">another_index</Link>
     </div>
   );
 }
 
-type Response = {
+type Result = {
   name: string;
-  sprites: {
-    back_default: string;
-  };
+};
+
+type Response = {
+  next: string;
+  count: number;
+  results: Result[];
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({
   locale,
 }) => {
   const { data } = await axios.get<Response>(
-    `https://pokeapi.co/api/v2/pokemon/pikachu`
+    `https://pokeapi.co/api/v2/pokemon`
   );
 
   // Pass data to the page via props
   const _props: Props = {
-    name: data.name,
-    imageUrl: data.sprites.back_default,
+    pokemons: data.results,
     ...(await serverSideTranslations(locale ?? "ja", ["common"])),
   };
   return { props: _props };
